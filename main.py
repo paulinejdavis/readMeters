b95291b8851c20cb4f774087bc35936377dcd590
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-29 17:28:11 +0100

    added main function that parses the entire dataset

--- main.py
+++ main.py
@@ -18,3 +18,91 @@ def count_meters(file_path):
                 meter_id = line.strip().split('|')[1]
                 meter_ids.add(meter_id)
     return len(meter_ids)
+
+ # `parse_file`defines the schema of the file to be parsed, which consists of a header,
+ # meter readings, and a footer. The structure dictionary maps the section names
+ # (i.e., 'HEADER', 'METER', 'READING', 'FOOTER') to their expected data types.
+
+
+def parse_file(file_path):
+    structure = {
+        'HEADER': str,
+        'METER': {
+            'meter_id': str
+        },
+        'READING': {
+            'reading_id': str,
+            'value': float,
+            'date': lambda x: datetime.strptime(x, '%Y%m%d').date(),
+            'status': str
+        },
+        'FOOTER': str
+    }
+
+    # Parse the file into a dictionary
+    data = {'meter_data': []}
+    footer = None
+    with open(file_path, 'r') as file:
+        meter_dict = {}
+        for line in file:
+            line = line.strip()
+            if not line:
+                continue
+            parts = line.split('|')
+            section = parts[0]
+            if section not in structure:
+                raise ValueError(f'Unknown section: {section}')
+            if section == 'METER':
+                meter_dict = {'meter_id': parts[1], 'readings': []}
+                data['meter_data'].append(meter_dict)
+            elif section == 'READING':
+                item = {}
+                for key, transform in structure[section].items():
+                    item[key] = transform(parts.pop(1))
+                meter_dict['readings'].append(item)
+            else:
+                if section == 'HEADER':
+                    data[section] = parts[1].upper()
+                elif section == 'FOOTER':
+                    footer = parts[1].upper()
+                else:
+                    data[section.lower()] = structure[section](parts[1])
+    if footer is not None:
+        data['FOOTER'] = footer
+
+    return data
+
+# `sum_valid_readings`` reads in the input file and counts the number of valid
+# readings. It looks for all sections labeled "READING" that contain a reading
+# value labeled "|V|".
+
+
+# main function takes in an input file, and allows the user to specify
+# whether they want to count the number of meters in the file or find
+# the sum of all valid meter readings in the file. If neither option
+# is specified, it simply parses the file and prints the resulting dictionary.
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Read meter readings from a file')
+    parser.add_argument('file_name', type=str, help='name of input file')
+    parser.add_argument('--count', action='store_true',
+                        help='display the number of meters in the file')
+    parser.add_argument('--valid', action='store_true',
+                        help='display the total sum of valid meter readings in the file')
+    args = parser.parse_args()
+
+    # Construct the full path to the file
+    file_path = f'./{args.file_name}'
+
+    if args.count:
+        meter_count = count_meters(file_path)
+        print(f'There are {meter_count} meters in the file.')
+    elif args.valid:
+        valid_sum = sum_valid_readings(file_path)
+        print(
+            f'The total sum of valid meter readings in the file is {valid_sum}.')
+    else:
+        data = parse_file(file_path)
+        pprint(data)
+        if 'FOOTER' in data:
+            print(f'FOOTER: {data["FOOTER"]}')
