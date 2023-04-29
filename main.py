1f830b415c2d996c67d818ef9d0073e98271e07f
Pauline Davis <pauljdavi.t21@btinternet.com>
2023-04-29 17:27:01 +0100

    add meter count function

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import argparse
 from datetime import datetime, date
 # import pprint module for pretty-printing data structures
 from pprint import pprint
+
+# `count_meters` counts the number of meters in the input
+# file by parsing the file and looking for the number of
+# meter sections in the file.
+
+
+def count_meters(file_path):
+    meter_ids = set()
+    with open(file_path, 'r') as file:
+        for line in file:
+            if 'METER|' in line:
+                meter_id = line.strip().split('|')[1]
+                meter_ids.add(meter_id)
+    return len(meter_ids)
